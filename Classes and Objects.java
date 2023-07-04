af4ad370661f50ba7cab8755c773568897f57f07
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-04 23:34:44 +0530

    Classes and Objects

---
+++ Classes and Objects.java
@@ -0,0 +1,48 @@
+public class OOPs {
+    public static void main(String args[]){
+          Pen P1 = new Pen(); // Created a pen object called P1
+          P1.setColor("Blue");
+          System.out.println(P1.color);
+          P1.setTip(5);
+          System.out.println(P1.tip);
+          P1.color = "Yellow";
+          System.out.println(P1.color);
+
+          BankAccount myAcc = new BankAccount();
+          myAcc.username = "rohitGoswami";
+          myAcc.setPassword("abcdefghi");
+
+    }
+}
+
+class BankAccount{
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage(int phy, int chem, int maths){
+        percentage = (phy + chem + maths) / 3;
+    }
+}
