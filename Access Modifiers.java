b5275d5dce4e0611874141b0f5f8508a55e54d2a
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-07 23:44:49 +0530

    Access Modifiers

--- Access Modifiers.java
+++ Access Modifiers.java
@@ -2,7 +2,7 @@ public class Access_Modifiers{
   public static void main(String args[]){
     BankAccount myAcc = new BankAccount(); //Bank account object for the access modifier example
     myAcc.username = "rohitGoswami";
-    myAcc.setPassword("abcdefghi"); //we can only change the password (which is private) by using fuction but we still can't access it
+    myAcc.setPassword("abcdefghi"); //we can only change the password(which is private) by using fuction but we still can't access it
     System.out.println(myAcc.username);
   }
 }
