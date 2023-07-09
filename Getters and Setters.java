bbc948eef616f24ec8bfb362eec0dc2f42c64a2f
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-09 22:51:14 +0530

    Getters and Setters

--- Getters and Setters.java
+++ Getters and Setters.java
@@ -1,6 +1,6 @@
 public class GettersAndSetters {
     public static void main(String args[]){
-        Pen p1 = new Pen();
+        Pen p1 = new Pen(); //'pen' object is created
         p1.setColor("Blue");
         System.out.println(p1.getColor());
         p1.setTip(5);
