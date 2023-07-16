113f56d11e69e3cec1562bfd9f822b513cd29b4b
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-16 23:36:51 +0530

    Concept of Copy Constructors in OOPs in Java

---
+++ Copy Constructors.java
@@ -0,0 +1,63 @@
+public class CopyConstructor {
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.name = "Rohit";
+        s1.roll = 34;
+        s1.password = "abcd";
+        s1.marks[0] = 78;
+        s1.marks[1] = 84;
+        s1.marks[2] = 90;
+
+
+        Student s2 = new Student(s1); //copying the properties of s1 into s2
+        s2.password = "xyz";
+        s1.marks[1]  = 45; //changing marks of s1, it should not be reflected but it will reflect due to concept of shallow and deep copying of data
+        for(int i = 0; i < 3; i++){
+            System.out.println(s2.marks[i]);
+        }
+
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+    String password;
+    int marks[];
+
+    //Sjallow Copy constructor
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks = s1.marks;
+    // }
+
+    //Deep Copy Constructor
+        Student(Student s1){
+            marks = new int[3];
+            this.name = s1.name;
+            this.roll = s1.roll;
+            for(int i = 0; i < marks.length; i++){
+                this.marks[i] = s1.marks[i];
+            }
+        }
+
+
+    Student(){
+        marks = new int[3];
+        System.out.println("this is a constructor....");
+    }
+
+    Student(String name){
+        marks = new int[3];
+        this.name = name;
+    }
+
+    Student(int roll){
+        marks = new int[3];
+        this.roll = roll;
+    }
+
+
+}
