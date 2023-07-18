5df59e4f44e1659aababdffb84ae1c67e6c31724
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-18 14:10:57 +0530

    Hierarchial Inheritance in OOPs in Java
    Types of Inheritance in OOPs

---
+++ Hierarchial Inheritance.java
@@ -0,0 +1,40 @@
+//single base class and multiple derived classes
+public class Hierarchial_Inheritance {
+    public static void main(String args[]){
+        Bird crow = new Bird();
+        crow.eat();
+        crow.breathe();
+        crow.fly();
+    }
+}
+
+//base class
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+
+class Mammal extends Animal{
+    void walk(){
+        System.out.println("walks");
+    }
+}
+
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swims");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("flies");
+    }
+}
