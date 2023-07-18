77ac1f4fe846e367aabffca4ffe4a4322ebcf107
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-18 15:51:19 +0530

    Method Overloading in OOPs
    Method Overloading is a type of Compile Time Polymorphism.

---
+++ Method Overloading.java
@@ -0,0 +1,22 @@
+public class Method_Overloading {
+    public static void main(String args[]){
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1,5));
+        System.out.println(calc.sum((float)1.6,(float)5.3));
+        System.out.println(calc.sum(1,5,9));
+    }
+}
+
+class Calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    float sum(float a, float b){
+        return a+b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
