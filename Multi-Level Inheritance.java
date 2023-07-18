58e2b7f025a41c944895a36837d1ff8822bfc28c
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-18 14:00:17 +0530

    Multi-Level Inheritance in OOPs in Java
    Types of Inheritance in OOPs

---
+++ Multi-Level Inheritance.java
@@ -0,0 +1,31 @@
+public class Multi_level_Inheritance {
+    public static void main(String args[]){
+        Dog kutta = new Dog();
+        kutta.eat();
+        kutta.legs = 4;
+        System.out.println(kutta.legs);
+    }
+}
+
+//base class
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+
+//derived class
+class Mammal extends Animal{
+    int legs;
+}
+
+//derived class
+class Dog extends Mammal{
+    String breed;
+}
