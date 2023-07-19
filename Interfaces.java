e273e26c9312439e6fdc9862200ae31cd292bc5b
Rohit Goswami <106897316+RohitGo8789@users.noreply.github.com>
2023-07-20 01:42:47 +0530

    Interfaces in OOPs

---
+++ Interfaces.java
@@ -0,0 +1,32 @@
+public class Interfaces {
+    public static void main(String args[]){
+        Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer{
+    void moves();
+}
+
+class Queen implements ChessPlayer{
+    public void moves(){
+        System.out.println("move up, down, left, right, diagonal (in all 4 directions)");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("move up, down, left, right");
+    }
+}
+
+class King implements ChessPlayer{
+    public void moves(){
+        System.out.println("move up, down, left, right, diagonal - (by 1 step)");
+    }
+}
+
+
+
+
